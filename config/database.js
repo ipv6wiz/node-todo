80ecbc468fb90a518056a8a53d038f1ca2fd0ac1
john <thejohnpage@gmail.com>
2016-11-11 16:56:33 -0800

    Changed  database.js added meanstacktutorials db to url and authSource admin to options

--- config/database.js
+++ config/database.js
@@ -1,11 +1,12 @@
 module.exports = {
     //remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
     remote: {
-        Url: 'mongodb://ec2-54-183-160-10.us-west-1.compute.amazonaws.com:27017/admin',
+        Url: 'mongodb://ec2-54-183-160-10.us-west-1.compute.amazonaws.com:27017/meanstacktutorials',
         Options: {
             user: 'madmin',
             pass: 'Plethy1320#dev2016',
-            authMechanism: 'SCRAM-SHA-1'
+            authMechanism: 'SCRAM-SHA-1',
+            authSource: "admin"
         }
     }
     ,local: {
