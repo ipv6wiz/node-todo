96e9c81d6b8312192868735307f8eb56af23fdf8
john <thejohnpage@gmail.com>
2016-11-11 16:38:18 -0800

    Changed  database.js added admin db to url

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
     //remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
     remote: {
-        Url: 'mongodb://ec2-54-183-160-10.us-west-1.compute.amazonaws.com:27017',
+        Url: 'mongodb://ec2-54-183-160-10.us-west-1.compute.amazonaws.com:27017/admin',
         Options: {
             user: 'madmin',
             pass: 'Plethy1320#dev2016',
