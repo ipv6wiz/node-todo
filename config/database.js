5ec3bf5935facc1ae0f7f74b504cb90ec50d5cc7
john <thejohnpage@gmail.com>
2016-11-11 16:34:52 -0800

    Changed  database.js removed db name defaulting to admin db

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
     //remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
     remote: {
-        Url: 'mongodb://ec2-54-183-160-10.us-west-1.compute.amazonaws.com:27017/meanstacktutorials',
+        Url: 'mongodb://ec2-54-183-160-10.us-west-1.compute.amazonaws.com:27017',
         Options: {
             user: 'madmin',
             pass: 'Plethy1320#dev2016',
